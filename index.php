e59592b0dd653564a29d059c3520eb80b67fa3fd
Leonardo Baccalario <leonardobaccalario@gmail.com>
2022-12-13 19:16:13 +0100

    finshed web site

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     </head>
     <body>
         <div class="bg-image"></div>
+
         <div class="title">
-            <img src="./image/title.png" alt="Lollapalooza">
+            <img src="./image/title.png" alt="Lollapalooza" width="800px" height="auto">
             <div class="buttons">
                 <button class="btn whatis"><a href="whatis.html">What is?</a></button>
-                <button class="btn experience"><a href="experience.html">Experience</a></button>
+                <button class="btn experience"><a href="experiences.html">Experiences</a></button>
                 <button class="btn lineup"><a href="lineup.html">Lineup</a></button>
             </div>
         </div>
+
         <div class="timeleft">
             <div class="text">There are</div>
             <div class="cards">
@@ -35,6 +37,10 @@
                 </div>
             </div>
             <div class="text">to the next Lollapalooza festival</div>
+        </div>
+        
+        <div class="logo">
+            <img src="./image/small_logo.png" alt="Lollapalooza" width="275px" height="auto">
         </div>
     </body>
 </html>
